bf5c45649d7b8b7d21f4b4a453ec198011534d9a
ghg <ghg@bas.ac.uk>
2017-05-11 14:33:13 +0100

    new fully tested version

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -1,7 +1,7 @@
 function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
-fprintf(CtrlVar.fidlog,' **************************     Úa version: 23 March 2017  ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************     Úa version: 11 May 2017  ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datestr(now));
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
