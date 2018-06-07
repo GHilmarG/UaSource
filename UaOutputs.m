76a5024e4938b562c1942c5069e01f372db98738
Hilmar Gudmundsson <GHilmarG@users.noreply.github.com>
2018-06-07 16:51:16 +0100

    corrected a mistake in the file naming when saving adapt mesh. New test on m in TestSlipperinessInputValues

--- UaOutputs.m
+++ UaOutputs.m
@@ -1,8 +1,26 @@
 function UserVar=UaOutputs(UserVar,CtrlVar,MUA,BCs,F,l,GF,InvStartValues,InvFinalValues,Priors,Meas,BCsAdjoint,RunInfo)
-
+%%
+% This routine is called during the run and can be used for saving and/or plotting data.
+% 
+%   UserVar=UaOutputs(UserVar,CtrlVar,MUA,BCs,F,l,GF,InvStartValues,InvFinalValues,Priors,Meas,BCsAdjoint,RunInfo)
+%
+% Write your own version of this routine and put it in you local run directory.
+% 
+% Inputs:
+% 
+%   BCs             Structure with all boundary conditions
+%   UaVars          A structure with fields such as
+%                   s, b, S, B, rho, rhow, ub, vb, ud, vd, AGlen, n , C, m , as, ab, g
+%   l               Lagrange parameters related to the enforcement of boundary
+%                   conditions.
+%   GF              Grounding floating mask for nodes and elements.
+%
+% If preferred to work directly with the variables rather than the respective fields of the structure 
+% `UaVars', then`UaVars' can easily be converted into variables using v2struc.
+%
+%%
 v2struct(F);
 time=CtrlVar.time; 
-
 plots='-plot-';
 
 if contains(plots,'-save-')
@@ -15,10 +33,7 @@ if contains(plots,'-save-')
     end
     
     if strcmp(CtrlVar.UaOutputsInfostring,'Last call')==0
-        
-        %
-        % 
-        %
+  
         
         FileName=sprintf('%s/%07i-Nodes%i-Ele%i-Tri%i-kH%i-%s.mat',...
             CtrlVar.Outputsdirectory,round(100*time),MUA.Nnodes,MUA.Nele,MUA.nod,1000*CtrlVar.kH,CtrlVar.Experiment);
