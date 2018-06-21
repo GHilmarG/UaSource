d05573fc242085370e7aaf4828eb13e6b528f6e3
G. Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2018-06-21 18:46:50 +0100

    UaCompendium and doc improvements

--- TracerConservationEquation.m
+++ TracerConservationEquation.m
@@ -1,7 +1,15 @@
 function [UserVar,c1,lambda]=TracerConservationEquation(UserVar,CtrlVar,MUA,dt,c0,u0,v0,a0,u1,v1,a1,kappa,BCsTracer)
 
-
-
+%
+% Solves the tracer conservation equation for the trace c on the form:
+%
+%  dc/dt + d (u c)/dx + d (v c)/dy - div (kappa grad c) = a
+%
+%
+% (Note: dc/dt is here the local time derivative, ie not the material derivative)
+%
+% The natural boundary condition is (grad c) \cdot \norm = 0, ie free outflow condition 
+%
 
 
 MLC=BCs2MLC(MUA,BCsTracer);
