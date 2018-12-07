f4e7fb6d33d98cd8b278e44f9595f4e8e163d2f8
ghg <ghg@bas.ac.uk>
2018-12-06 17:03:03 -0800

    M0difyColormap does + and - ranges

--- UaUtilities/ModifyColormap.m
+++ UaUtilities/ModifyColormap.m
@@ -5,8 +5,13 @@ function ModifyColormap
 
 [t1,t2]=caxis ;
 range=(t2-t1)*linspace(0,1,size(colormap,1))+t1 ;
-%I=abs(range)<0.3;
-[~,I]=min(abs(range));
+
+
+temp=range ; temp(temp>0)=NaN;
+[~,I1]=max(temp);
+temp=range ; temp(temp<0)=NaN;
+[~,I2]=min(temp);
+I=[I1;I2];
 temp=colormap;
 temp(I,:)=temp(I,:)*0+0.9;
 colormap(gca,temp)
