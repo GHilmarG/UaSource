748a3159a6d38412551126937dfcf1a0cd6c455c
G. Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2019-08-13 18:52:00 +0100

    decomposition

--- TestABxyDirectElimination.m
+++ TestABxyDirectElimination.m
@@ -1,20 +1,20 @@
 %%
-n=4;
+n=5;
 p=2;
 
 A=rand(n,n) ; A=A+A' ; 
-B=eye(p,n)  ;
+L=eye(p,n)  ;
 
 f=ones(n,1) ; g=ones(p,1) ;
 
 
-T=[A B' ; B zeros(p,p)];
+T=[A L' ; L zeros(p,p)];
 
 sol=T\[f;g ] ; x=sol(1:n) ; y=sol(n+1:end) ; 
 
 
-Q=(eye(n,n)-B'*B) ; 
-xTest=(Q*A+ B'*B) \ (Q *f + B'*g)
+Q=(eye(n,n)-L'*L) ; 
+xTest=(Q*A+ L'*L) \ (Q *f + L'*g)
+(Q*A+ L'*L)
 
-format long g
 [x(:) xTest(:)]
