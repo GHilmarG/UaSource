96b67feb6014576f20ad45b29d13eecb1560f936
G. Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2020-02-05 16:59:58 +0000

    PrintRunInfo

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -2,7 +2,7 @@
 function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
-fprintf(CtrlVar.fidlog,' **************************     Úa version: beta 5 February 2020  ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************     Úa version: beta 5 February, 2020  ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datestr(now));
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
