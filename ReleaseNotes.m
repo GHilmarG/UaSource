0dbfc4b5dbbff7a4f766c77a1902082a7482de1e
G. Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2020-02-12 13:53:35 +0000

    Release Notes

--- ReleaseNotes.m
+++ ReleaseNotes.m
@@ -2,41 +2,40 @@
 
 
 
-
-% 
 %%
 % *Release Notes*
 % _February 2020_
 %
 %
-% Úa can now be called with CtrlVar as second argument, e.g 
+% * Úa can now be called with CtrlVar as second argument, e.g 
 %
 %   Ua([],CtrlVar) 
 %
-% in which case
-% the fields of the CtrlVar on input will be those used in the run even if these same
-% fields are also defined in Ua2D_InitialUserInput.m
+% in which case the fields of the CtrlVar on input will be those used in the run
+% even if these same fields are also defined in Ua2D_InitialUserInput.m
 % 
-% The combination of local mesh refinement using the newest-vertex bisection method with
+% * The combination of local mesh refinement using the newest-vertex bisection method with
 % mesh deactivation is now very flexible and allows for both reduction and extension of the
 % computational domain during the run.
 % 
-% Several new sliding laws are now implemented. These include Weertman, Coulomb, Budd, Tsai,
+% * Several new sliding laws are now implemented. These include Weertman, Coulomb, Budd, Tsai,
 % Cornford, and  Umbi. Refer to the Ua compendium for definition of these
 % different sliding laws. Inversion is also possible for all these sliding laws except for
 % the Coulomb sliding.
 % 
-% The semi-implicit time-stepping method now uses SUPG by default and when used the
+% * The semi-implicit time-stepping method now uses SUPG by default and when used the
 % automated time-stepping algorithm ensures that time step fulfils the CFL condition.
 % 
-% When using automated mesh refinement/coarsening in a time-dependent run, the upper
+% * When using automated mesh refinement/coarsening in a time-dependent run, the upper
 % surfaces are mapped between meshes and the thickness calculated afterwards. (Previously,
 % the thickness was mapped and the surface calculated afterwards.) This may cause a
 % potential violation of mass conservation. However, this also ensures smooth upper
 % surfaces even when refining mesh over areas with very uneven bedrock topography.
 % 
+% * Inversion can be done using all implemented sliding laws except Coulumb. 
+% 
 %
-% u, v and h residuals now calulated in the L2 norm instead of the l2 norm as in the
+% * u, v and h residuals now calulated in the L2 norm instead of the l2 norm as in the
 % past. 
 %
 % *New user input file options:*
@@ -60,8 +59,4 @@
 % additional criterion.
 % 
 % 
-% Inversion can be done using all implemented sliding laws except Coulumb. 
-% 
-% 
-%
-% 
+%%
