feb3807c2dfd363513caf3bb548eb9e497a7d26b
G. Hilmar Gudmundsson <hilmar.gudmundsson@northumbria.ac.uk>
2020-02-21 13:30:18 +0000

    Less print during run

--- Ua2D.m
+++ Ua2D.m
@@ -42,32 +42,8 @@ Ruv=[];
 
 WallTime0=tic;
 %% Clear any persistent variables
-clear AdaptiveTimeStepping
-clear AdaptMesh
-clear BCs2MLC
-clear CostFunctionValueAndGradient
-clear EleAverageInterpolate
-clear JGH
-clear NrOfIntegrationPoints
-clear LocalMeshRefinement
-clear MeshAdvanceRetreat
-clear Mesh2dEleSizeFunction
-clear multiWaitbar
-clear NewConjugatedGrad
-% User input files
-clear DefineSlipperyDistribution
-clear DefineAGlenDistribution
-clear DefineGeometry
-clear DefineInputsForInverseRun
-clear DefineDensities
-clear DefineDesiredEleSize
-clear DefineBoundaryConditions
-clear DefineMassBalance
-clear UaOutputs
-% Ua utilities
-clear PlotMeshScalarVariable 
-clear PlotFEmesh
-clear FindOrCreateFigure
+ClearPersistentUaVariables();
+
 
 %% Define CtrlVar
 
