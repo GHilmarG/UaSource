2747b1a3a88f4a1a0f9353a35671c46cb9476e19
ghg <ghg@bas.ac.uk>
2020-02-24 22:08:40 +0000

    Test Block Solve

--- TestSolveBlockStructure.m
+++ TestSolveBlockStructure.m
@@ -1,48 +1,75 @@
 %%
 %%
 fprintf('\n\n\n')
-n=350 ;
-p=200;
+n=3 ;
+p=2;
 
 A=sprandsym(n,n,0.01);
+A=sparse([1 0.01 0 ; 0.01 1 0 ; 0 0 1]);
 
-L=zeros(p,n) ;
+
+B=zeros(p,n) ;
 
 for I=1:p
-    L(I,I)=1;
+    B(I,I)=1;
 end
 
-B=zeros(p,p);
-AA=[A L' ; L B ]  ;
+C=zeros(p,p);
+AA=[A B' ; B C ]  ;
 
-b=zeros(n,1)+1;
-c=zeros(p,1)+1;
+f=zeros(n,1)+1;
+g=zeros(p,1)+1;
 
 % Null-space method
 tic
-Z=null(L);
-xhat=L\c ;
-z= (Z'*A*Z)\(Z'*(b-A*xhat));
+Z=null(B);
+xhat=B\g ;
+z= (Z'*A*Z)\(Z'*(f-A*xhat));
 x=Z*z+xhat;
-lambda=L' \ (b-A*x) ;
+y=B' \ (f-A*x) ;
 toc
 
-fprintf('%g \t %g \n ',norm(A*x+L'*lambda-b),norm(L*x-c))
+fprintf('%g \t %g \n ',norm(A*x+B'*y-f),norm(B*x-g))
 
 % Range-space method
 tic
-lambda= L*(A\L')\(L*(A\b)-c) ;
-x=A\(b-L'*lambda) ;
+y= B*(A\B')\(B*(A\f)-g) ;
+x=A\(f-B'*y) ;
 toc
 
 
-% tic
-% iA=inverse(factorize(A)) ;
-% lambda= L*(iA*L')\(L*(iA*b)-c) ;
-% x=iA*(b-L'*lambda) ;
-% toc
 
-fprintf('%g \t %g \n ',norm(A*x+L'*lambda-b),norm(L*x-c))
+%  R=chol(A)        % A = R'*R    ->  A x = b ; 
+%                                  % x = A\b 
+%                                  % x = R\(R'\b)
+%                                  % A\b = (R\R'\b)
+% 
+% R=chol(A) ;                          
+%  [R,flag,p]= chol (A, 'vector') ->  A(p,p) = R'*R  
+%  [R,flag,P]= chol (A )          -> P'*A*P = R'*R  -> A\b = (
+%                            
+%
+%
+% [R,flag,P]= chol (A); 
+% sol=A\f ; 
+% sol2=P*(R\(R'\(P'*f))) ; 
+% norm(sol-sol2)
+
+
+%% this works for chol and sparse
+ tic
+ 
+ % y= B*(R\(R'\B'))\(B*(R\(R'\f))-g) ;
+ % x= R\(R'\(f-B'*y)) ;
+ [R,flag,P]= chol (A); 
+ y= B*P*(R\(R'\(P'*B')))\(B*P*(R\(R'\(P'*f)))-g) ;
+ x= P*(R\(R'\(P'*f-P'*B'*y))) ;
+ 
+ toc                              
+
+fprintf('%g \t %g \n ',norm(A*x+B'*y-f),norm(B*x-g))
+%%
+
 
 tic
 CtrlVar.ALSIterationMin=1;
@@ -51,6 +78,7 @@ CtrlVar.ALSpower=5; CtrlVar.Solve.LUvector=true;
 CtrlVar.InfoLevelLinSolve=1;
 CtrlVar.TestForRealValues=true;
 CtrlVar.LinSolveTol=1e-6;
-[x,lambda] = AugmentedLagrangianSolver(A,L,b,c,c*0,CtrlVar);
+[x,y] = AugmentedLagrangianSolver(A,B,f,g,g*0,CtrlVar);
 toc
-fprintf('%g \t %g \n ',norm(A*x+L'*lambda-b),norm(L*x-c))
+fprintf('%g \t %g \n ',norm(A*x+B'*y-f),norm(B*x-g))
+
