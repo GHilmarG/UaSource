3c152803370daa84aa1420459f0661cdf2485138
U-KATLA\Hilmar <ghg@bas.ac.uk>
2020-07-21 16:07:16 +0100

    RI

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -2,7 +2,7 @@
 function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
-fprintf(CtrlVar.fidlog,' **************************     Úa version: beta 14 February, 2020  ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************     Úa version: beta 21 July, 2020  ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datestr(now));
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
