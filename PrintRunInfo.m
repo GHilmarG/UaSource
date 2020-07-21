d5aaff66f98f99292a03549e8bf4b782b39184ca
U-KATLA\Hilmar <ghg@bas.ac.uk>
2020-07-21 17:36:56 +0100

    PI

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -2,7 +2,7 @@
 function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
-fprintf(CtrlVar.fidlog,' **************************     Úa version: 29 May 2020  ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************     Úa version: 21 July 2020 (master)  ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datestr(now));
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
