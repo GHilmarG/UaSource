368400d271c998a5fc32371ef5808044e2159e2d
ghg <ghg@bas.ac.uk>
2021-06-29 10:13:07 +0100

    VAF returns more outputs fields

--- UaUtilities/CalcVAF.m
+++ UaUtilities/CalcVAF.m
@@ -1,37 +1,47 @@
-function [VAF,IceVolume,GroundedArea]=CalcVAF(CtrlVar,MUA,h,B,S,rho,rhow,GF)
+function [VAF,IceVolume,GroundedArea,hAF,hfPos]=CalcVAF(CtrlVar,MUA,h,B,S,rho,rhow,GF)
 
 %%
+%
+%   [VAF,IceVolume,GroundedArea,hAF,hfPos]=CalcVAF(CtrlVar,MUA,h,B,S,rho,rhow,GF)
+%
 % Calculates volume above flotation, and optionally ice volume and grounded area
 %
-% GF is only needed to calculate volume and grounded area. 
+% GF is only needed to calculate grounded area.
 %
 %
 % To calculate a rough estimate of resulting change in mean sea level, divide the change
 % in VAF with the area of the ocean (3.625e14 m^2).
+%
+%
+%   VAF       :  Volume above flotation
+%   IceVolume :  Total ice volume withing the domain, i.e. including areas that are afloat.
+%   hAF       :  (postive) ice thickness above floation
+%   hfPOs     :  (positive) flotation thickness (also somtimes referred to as floation profile). Where h>fhPos, the ice is grounded.
+%
 %%
 
 narginchk(7,8)
-nargoutchk(1,3)
+nargoutchk(1,5)
 
 
-% One option: 
+% One option:
 %
 % hf=rhow*(S-B)./rho ;
-% hAF=h*0 ;                                   % hAF : ice-thickness above flotation. 
-% 
+% hAF=h*0 ;                                   % hAF : ice-thickness above flotation.
+%
 % isBgtS= B > S ;  hAF(isBgtS)=h(isBgtS) ;    % grounded above sea level, full contribution to hAF
 % I=~isBgtS & ~ishlthf ; hAF(I)=h(I)-hf(I) ;  % grounded below sea level some contribution to hAF
 % ishlthf = (h < hf)  ; hAF(ishlthf)=0   ;    % not grounded, no contribution to hAF
-% 
-% 
-% or simply: 
-hfPos=(S>B).*rhow.*(S-B)./rho ; % (positive) flotation thickness
-hAF= (h>hfPos).*(h-hfPos) ;                % ice thickness above floatation
+%
+%
+% or simply:
+hfPos=(S>B).*rhow.*(S-B)./rho ;            % (positive) flotation thickness
+hAF= (h>hfPos).*(h-hfPos) ;                % (positive) ice thickness above floatation
 
 
-VAF.node=hAF.*rho./rhow ;               % thickness above flotation in water eq. 
-VAF.ele=FEintegrate2D([],MUA,VAF.node); % VAF for each element (m^3) 
-VAF.Total=sum(VAF.ele);                 % total volume above flotation over the whole model domain 
+VAF.node=hAF.*rho./rhow ;               % thickness above flotation in water eq.
+VAF.ele=FEintegrate2D([],MUA,VAF.node); % VAF for each element (m^3)
+VAF.Total=sum(VAF.ele);                 % total volume above flotation over the whole model domain
 
 
 if nargout>1
