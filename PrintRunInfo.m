afa6e2c40b98ad8eedcd885b677537ab62668e6e
ghg@bas.ac.uk <ghg@bas.ac.uk>
2021-11-01 17:56:43 +0000

    PRI updated

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -2,7 +2,7 @@
 function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
-fprintf(CtrlVar.fidlog,' **************************     Úa version: 20 November 2020 (master)  ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************     Úa version: 1 November 2021 (master)  ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datestr(now));
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
