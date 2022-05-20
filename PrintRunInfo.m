5bba8705fc5ee9b89d99d50ca99ab562c2df285f
ghg@bas.ac.uk <ghg@bas.ac.uk>
2022-05-20 14:16:15 +0100

    PrintRunInfo updated ahead of merge

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -3,7 +3,7 @@ function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
 
-fprintf(CtrlVar.fidlog,' **************************     Úa version: 1  November, 2021  (beta) ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************     Úa version: 21  May, 2022  (beta) ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datestr(now));
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
