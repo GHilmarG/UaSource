f748f93f7558f30f43a6c6baaaff79ec0b40215f
U-C23000099\pcnj6 <ghg@bas.ac.uk>
2023-03-01 19:22:37 +0000

    version info

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -2,7 +2,7 @@
 function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
-fprintf(CtrlVar.fidlog,' **************************     Úa version: 26 July 2022 (alpha)  ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************     Úa version: 01 March 2023 (hsolve)  ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datestr(now));
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
