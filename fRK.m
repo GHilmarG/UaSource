5d0218df7e67d9e52f1d5ff08eb829706e9bf675
ghg@bas.ac.uk <ghg@bas.ac.uk>
2023-07-24 13:18:37 +0100

    fRK updated

--- fRK.m
+++ fRK.m
@@ -1,20 +1,37 @@
 function [R,K]=fRK(x,problemtype)
 
-x=x(:) ; 
+x=x(:) ;
 
 switch problemtype
 
     case "[x1,x2]"
 
         R=x + 1 ;
-        K=eye(numel(x)) ; 
+        K=eye(numel(x)) ;
 
     case "[x1+x2,x2]"
-        
-      R(1)=x(1)+x(2);
-      R(2)=x(2);
-      R=R(:) ; 
-      K=[1  1 ; 0 1] ;
+
+        R(1)=x(1)+x(2);
+        R(2)=x(2);
+        R=R(:) ;
+        K=[1  1 ; 0 1] ;
+
+    case "[x1^2,x2]"
+
+        R(1)=x(1)^2;
+        R(2)=x(2);
+        R=R(:) ;
+        K=[2*x(1)  0 ; 0 1] ;
+
+
+    case "[x1^2+x2,x2]"
+
+        R(1)=x(1)^2+x(2);  
+        R(2)=x(2);
+        R=R(:) ;
+
+        K=[2*x(1)  1 ; ...   % \nabla R1^T
+            0     1] ;       % \nabla R2^T
 
 
     otherwise
