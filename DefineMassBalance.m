547fbe9c3cc452e9727e48e849ed1f5dc404d94f
U-C23000099\pcnj6 <ghg@bas.ac.uk>
2023-10-26 17:37:08 +0100

    Release notes and some help info updated

--- DefineMassBalance.m
+++ DefineMassBalance.m
@@ -18,7 +18,7 @@ function [UserVar,as,ab]=DefineMassBalance(UserVar,CtrlVar,MUA,F)
 % dasdh and dabdh only need to be specified if the mass-balance feedback option is
 % being used. 
 %
-% In Úa the mass balance, as returned by this m-file, is multiplied internally by the local ice density. 
+% In Ãša the mass balance, as returned by this m-file, is multiplied internally by the local ice density. 
 %
 % The units of as and ab are water equivalent per time, i.e. usually
 % as and ab will have the same units as velocity (something like m/yr or m/day).
@@ -41,24 +41,32 @@ function [UserVar,as,ab]=DefineMassBalance(UserVar,CtrlVar,MUA,F)
 % These fields need to be returned at the nodal coordinates. The nodal
 % x and y coordinates are stored in MUA.coordinates, and also in F as F.x and F.y
 %
-% Examples:  
 %
-% To set upper surface mass balance to zero, and melt along the lower ice
-% surface to 10 over all ice shelves:
+% *Examples* : 
+%
+% *To set upper surface mass balance to zero, and melt along the lower ice
+% surface to 10 over all ice shelves:* 
 %
 %   as=zeros(MUA.Nnodes,1);
 %   ab=-(1-F.GF.node)*10 
 %
 %
-% To set upper surface mass balance as a function of local surface elevation and
-% prescribe mass-balance feedback for the upper surface:
+% *To set upper surface mass balance as a function of local surface elevation and
+% prescribe mass-balance feedback for the upper surface:* 
 %
 %   as=0.1*F.h+F.b;
 %   dasdh=zeros(MUA.Nnodes,1)+0.1;
 %   ab=F.s*0;
 %   dabdh=zeros(MUA.Nnodes,1);
 %
+% *To add basal melt due to sliding as a mass-balance term:* 
+%
+%   [tbx,tby,tb,beta2] = CalcBasalTraction(CtrlVar,MUA,F.ub,F.vb,F.C,F.m,F.GF);
+%   L=333.44 ;                                        % Enthalpy of fusion = L = [J/gramm = kJ/kg]  Make sure that the units are correct.
+%   F.ab=-(tbx.*F.ub+tby.*F.vb)./(F.rho.*L);          % Ablation (Melt) is always negative, accumulation is positive
+%
 % 
+%
 %%
 
 
