3cb356a096dfff7bf4195af686810d7ceaea93c0
U-C23000099\pcnj6 <ghg@bas.ac.uk>
2024-05-01 10:55:36 +0100

    runinfo updated ahead of merge with beta

--- PrintRunInfo.m
+++ PrintRunInfo.m
@@ -2,7 +2,7 @@
 function PrintRunInfo(CtrlVar)
 
 fprintf(CtrlVar.fidlog,'\n \n');
-fprintf(CtrlVar.fidlog,' **************************    Úa version: 17 March 2024 (master)  ********************** \n');
+fprintf(CtrlVar.fidlog,' **************************    Ãša version: 1 May 2024 (master)  ********************** \n');
 fprintf(CtrlVar.fidlog,'    Run starts at %s  \n ',datetime);
 fprintf(CtrlVar.fidlog,'   Experiment: %s  \n ',CtrlVar.Experiment);
 
