ec6b17e7007286003eba8703ec90464a838b26f7
ghg@bas.ac.uk <ghg@bas.ac.uk>
2024-05-20 14:29:50 +0100

    by a mistake quad degree=25 had been entered in QuadratureRuleDegree for testing puposes and not delted. Now back to Degree=4 as default for lin elements

--- FE_inner_product.m
+++ FE_inner_product.m
@@ -32,21 +32,32 @@ nf=numel(f) ; ng=numel(g) ;
 
 [nM,mM]=size(M);
 
+theta=nan;
+
 if nM==nf
-    
+
     fg=f'*M*g;   % = (M*f)' * g
-    fNorm=sqrt(f'*M*f) ; 
-    gNorm=sqrt(g'*M*g) ; 
-    theta=acos(fg/(fNorm*gNorm)) ; 
-    
+
+    if nargout>1
+        fNorm=sqrt(f'*M*f) ;
+        gNorm=sqrt(g'*M*g) ;
+        theta=acos(fg/(fNorm*gNorm)) ;
+    else
+        theta=nan;
+    end
+
 elseif nf==2*nM
-    
+
     fg=f(1:nM)'*M*g(1:nM)+f(1+nM:nf)'*M*g(1+nM:ng);
-    
+
+    if nargout>1
     fNorm=sqrt(f(1:nM)'*M*f(1:nM)+f(1+nM:nf)'*M*f(1+nM:nf)) ;
     gNorm=sqrt(g(1:nM)'*M*g(1:nM)+g(1+nM:ng)'*M*g(1+nM:ng)) ;
-    theta=acos(fg/(fNorm*gNorm)) ; 
-    
+    theta=acos(fg/(fNorm*gNorm)) ;
+    else
+        theta=nan;
+    end
+
 end
 
 
